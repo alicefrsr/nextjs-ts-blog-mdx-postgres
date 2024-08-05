2f42ee32ce71b6208a2a445900d3b8ae8da2a8ae
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-08-05 23:28:44 +0200

    set up shadcn ui, main nav menu, icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import LatestPosts from '@/components/home/LatestPosts';
+import { MainNav } from '@/components/MainNav';
 
 export default function Home() {
-  return <LatestPosts />;
+  return (
+    <>
+      <MainNav />
+      <main>
+        <div>
+          <LatestPosts />;
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ src/components/MainNav.tsx
@@ -0,0 +1,177 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+
+import { cn } from '@/lib/utils';
+// import { Icons } from '@/components/icons';
+import { Icons } from './icons';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+
+const posts: { title: string; href: string; description: string }[] = [
+  {
+    title: 'React',
+    href: '/blog/react',
+    description: 'Stuff about React.',
+  },
+  {
+    title: 'Next.js',
+    href: '/blog/next-js',
+    description: 'Stuff about Next.js.',
+  },
+  {
+    title: 'CSS',
+    href: '/blog/css',
+    description: 'Stuff about css.',
+  },
+  {
+    title: 'Javacript',
+    href: '/blog/javacript',
+    description: 'Stuff about Javascript.',
+  },
+  {
+    title: 'SEO',
+    href: '/blog/seo',
+    description: 'Stuff about SEO.',
+  },
+
+  {
+    title: 'Random',
+    href: '/blog/random',
+    description: 'Random stuff.',
+  },
+];
+
+export function MainNav() {
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-start justify-start md:flex-row md:items-center md:justify-between pt-10 z-50'
+      )}
+    >
+      <Link href={'/'}>
+        <div className='flex items-center justify-between w-32 '>
+          {/* LOGO */}
+          <Icons.logo className='h-6 w-6' />
+          <p>Huggabug</p>
+        </div>
+      </Link>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+                <li className='row-span-3'>
+                  <NavigationMenuLink asChild>
+                    <a
+                      className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                      href='/'
+                    >
+                      {/* <Icons.logo className='h-6 w-6' /> */}
+                      <div className='mb-2 mt-4 text-lg font-medium'>
+                        What's this?
+                      </div>
+                      <p className='text-sm leading-tight text-muted-foreground'>
+                        A selection of articles about whatever it is I've
+                        learned at some point in my coding journey that I feel
+                        may be of use to others.
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <ListItem href='/docs' title='Menu item'>
+                  Menu Item title
+                </ListItem>
+                <ListItem href='/docs/installation' title='Menu item'>
+                  Menu Item title
+                </ListItem>
+                <ListItem href='/docs/primitives/typography' title='Menu item'>
+                  Menu Item title
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Posts</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
+                {posts.map((post) => (
+                  <ListItem
+                    key={post.title}
+                    title={post.title}
+                    href={post.href}
+                  >
+                    {post.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href='/about' legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          {/* <NavigationMenuItem>
+            <Link href='/about' legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href='/about' legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem> */}
+          <div className='flex items-center justify-between w-20'>
+            {/* <ModeToggle /> */}
+            {/* RSS */}
+            <Link href='/rss'>
+              <Icons.rss className='h-6 w-6' />
+            </Link>
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className
+          )}
+          {...props}
+        >
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = 'ListItem';
