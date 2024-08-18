358dc2f6f1d31e1b4a5d3981ebfd416b423d94e2
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-08-18 20:16:08 +0200

    commit

---
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import Header from '@/components/header';
+import { formatDate, getBlogPosts } from '../../utils';
+import Container from '@/components/Container';
+import { BreadcrumbWithCustomSeparator } from '@/components/breadcrumb';
+
+export default function SlugPage({
+  params,
+}: {
+  category: string;
+  slug: string;
+}) {
+  let post = getBlogPosts().find((post) => post.slug === params.slug);
+  console.log(post);
+
+  if (!post) {
+    notFound();
+  }
+  return (
+    <>
+      <Header>
+        <Container>
+          <BreadcrumbWithCustomSeparator
+            category={post?.metadata.category}
+            slug={post?.slug}
+          />
+          <h1 className=' font-semibold text-2xl tracking-tighter mt-4'>
+            {post?.metadata.title}
+          </h1>
+          <div className='flex justify-between items-center mt-2 mb-4 text-sm'>
+            <p className='text-sm text-neutral-600 dark:text-neutral-400 mt-2 '>
+              {formatDate(post?.metadata.publishedAt)}
+            </p>
+          </div>
+        </Container>
+      </Header>
+      <Container>
+        <article className='prose'>
+          <CustomMDX />
+        </article>
+      </Container>
+    </>
+  );
+}
